9b9075e2b45a8e59dc19d682da4af2d56562d65a
Батсүх Мөнхцог <HP233131@M301-CC0123.local>
2023-08-02 15:17:44 +0800

    s

--- Gallery.jsx
+++ Gallery.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect , useRef } from 'react';
-import { StyleSheet ,Text, View, Button, Image, TouchableOpacity , ScrollView} from 'react-native';
+import { StyleSheet ,Text, View, Button, Image, TouchableOpacity , ScrollView , FlatList} from 'react-native';
 import { Video} from 'expo-av';
 
 export default function Gallery({route,navigation}) {
@@ -15,52 +15,93 @@ export default function Gallery({route,navigation}) {
         // });
     }
 
-    return (
-        <ScrollView style={styles.container}>
-            {
-                files && files.assets.map((file,i)=>{
-                    return(
-                        file.mediaType=="video" ?
-                        <TouchableOpacity key={i} onPress={()=>selectFile(file)}>
-                            <Video
-                                ref={video}
-                                style={styles.video}
-                                source={{uri:file && file.uri}}
-                                useNativeControls
-                                resizeMode="cover"
-                                // isLooping
-                                // onPlaybackStatusUpdate={status => setStatus(() => status)}
-                            />
-                        </TouchableOpacity>
-                        :
+    // console.log(files)
 
-                        <TouchableOpacity key={i} onPress={()=>selectFile(file)}>
-                            <Image
-                                source={{uri:file && file.uri}}
-                                style={styles.image}
-                            />
-                        </TouchableOpacity>
-                    )
-                })
-            }
-        </ScrollView>
+    const RenderFile=({item})=>{
+        //prop's name can only be given as "item"
+        return(
+            item?.mediaType=="video" ?
+                <TouchableOpacity onPress={()=>selectFile(item)}>
+                    <Video
+                        ref={video}
+                        style={styles.video}
+                        source={{uri:item && item.uri}}
+                        useNativeControls
+                        resizeMode="cover"
+                        // isLooping
+                        // onPlaybackStatusUpdate={status => setStatus(() => status)}
+                    />
+                </TouchableOpacity>
+            :
+
+            <TouchableOpacity onPress={()=>selectFile(item)}>
+                <Image
+                    source={{uri:item && item.uri}}
+                    style={styles.image}
+                />  
+            </TouchableOpacity>
+        )
+    }
+
+    return (
+        // <ScrollView style={styles.container}>
+        //     {
+        //         files && files.assets.map((file,i)=>{
+        //             return(
+        //                 file.mediaType=="video" ?
+        //                 <TouchableOpacity key={i} onPress={()=>selectFile(file)}>
+        //                     <Video
+        //                         ref={video}
+        //                         style={styles.video}
+        //                         source={{uri:file && file.uri}}
+        //                         useNativeControls
+        //                         resizeMode="cover"
+        //                         // isLooping
+        //                         // onPlaybackStatusUpdate={status => setStatus(() => status)}
+        //                     />
+        //                 </TouchableOpacity>
+        //                 :
+
+        //                 <TouchableOpacity key={i} onPress={()=>selectFile(file)}>
+        //                     <Image
+        //                         source={{uri:file && file.uri}}
+        //                         style={styles.image}
+        //                     />
+        //                 </TouchableOpacity>
+        //             )
+        //         })
+        //     }
+        // </ScrollView>
+        <FlatList
+            data={files?.assets}
+            // renderItem={({file}) => <RenderFile file={file} />}
+            renderItem={RenderFile}
+            numColumns={3}
+            keyExtractor={file=>file.uri}
+            key={file=>file.uri}
+            // style={styles.container}
+            contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
+        />
+        // <Text>aa</Text>
     );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    flexDirection:"row",
-    flexWrap:"wrap"
+    // flexDirection:"column",
+    // flexWrap:"wrap"
   },
   image:{
     width:100,
     height:100,
     resizeMode:"cover",
-    alignSelf:"stretch"
+    alignSelf:"stretch",
+    margin:5
   },
   video:{
     width:100,
-    height:100
+    height:100,
+    margin:5
   }
 });
